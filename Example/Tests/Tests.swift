a47afbb5abdf955ef739808cc98c308e03e24778
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-01-16 12:54:22 +0900

    add test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,26 +3,137 @@ import XCTest
 import Yashin
 
 class Tests: XCTestCase {
-    
+
+    var radarChart = Yashin()
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func testValidateMinAndMax() {
+
+        radarChart.set(
+            ["aaa", "bbb", "ccc",],
+            [
+                ([4, 5, 8], UIColor.greenColor().colorWithAlphaComponent(0.40)),
+                ([10, 3, 6], UIColor.darkGrayColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateMinAndMax() == true, "when default minValue and maxValue")
+
+        radarChart.minValue = 10
+        radarChart.maxValue = 0
+
+        XCTAssert(radarChart.validateMinAndMax() == false, "when minValue is more than maxValue")
+
+        radarChart.maxValue = 10
+
+        XCTAssert(radarChart.validateMinAndMax() == false, "when minValue equals maxValue")
+
+        radarChart.maxValue = 100
+
+        XCTAssert(radarChart.validateMinAndMax() == true, "when minValue is less than maxValue")
+    }
+
+    func testValidateKeyCount() {
+
+        radarChart.set(
+            ["aaa", "bbb"],
+            [
+                ([4, 5], UIColor.greenColor().colorWithAlphaComponent(0.40)),
+                ([10, 3], UIColor.darkGrayColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateKeyCount() == false, "when key count is less than 3")
+
+        radarChart.set(
+            ["aaa", "bbb", "ccc"],
+            [
+                ([4, 5, 8], UIColor.greenColor().colorWithAlphaComponent(0.40)),
+                ([10, 3, 6], UIColor.darkGrayColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateMinAndMax() == true, "when key count is more than 2")
+    }
+
+    func testValidateIntegrityKeyAndValue() {
+
+        radarChart.set(
+            ["aaa", "bbb", "ccc"],
+            [
+                ([4, 5], UIColor.greenColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateIntegrityKeyAndValue() == false, "when key and value integrity is wrong (single data) [1]")
+
+        radarChart.set(
+            ["aaa", "bbb"],
+            [
+                ([4, 5, 8], UIColor.greenColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateIntegrityKeyAndValue() == false, "when key and value integrity is wrong (single data) [2]")
+
+        radarChart.set(
+            ["aaa", "bbb", "ccc"],
+            [
+                ([4, 5, 8], UIColor.greenColor().colorWithAlphaComponent(0.40)),
+                ([10, 3], UIColor.darkGrayColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateIntegrityKeyAndValue() == false, "when key and value integrity is wrong (multi data)")
+
+        radarChart.set(
+            ["aaa", "bbb", "ccc"],
+            [
+                ([4, 5, 8], UIColor.greenColor().colorWithAlphaComponent(0.40)),
+                ([10, 3, 6], UIColor.darkGrayColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateIntegrityKeyAndValue() == true, "when key and value integrity is correct")
+    }
+
+    func testValidateSufficientFrame() {
+
+        radarChart = Yashin(frame: CGRectMake(0, 0, 100, 100))
+
+        radarChart.set(
+            ["aaa", "bbb"],
+            [
+                ([4, 5], UIColor.greenColor().colorWithAlphaComponent(0.40)),
+                ([10, 3], UIColor.darkGrayColor().colorWithAlphaComponent(0.40))
+            ]
+        )
+
+        XCTAssert(radarChart.validateSufficientFrame() == false, "when frame is smaller than expected")
+
+        radarChart.frame = CGRectMake(0, 0, 180, 180)
+
+        XCTAssert(radarChart.validateSufficientFrame() == true, "when frame is big enough [1]")
+
+        radarChart.padding = 100
+
+        XCTAssert(radarChart.validateSufficientFrame() == false, "when frame is smaller than expected (after padding is changed)")
+
+        radarChart.frame = CGRectMake(0, 0, 260, 260)
+
+        XCTAssert(radarChart.validateSufficientFrame() == true, "when frame is big enough [2]")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testPerformanceDraw() {
         self.measureBlock() {
-            // Put the code you want to measure the time of here.
+            //
         }
     }
     
